cfaa351fa9d9bbf41060e16e3d74510e5ecc31a9
AbdelrahmanH161 <geo.abdelrahmanem@gmail.com>
2022-10-05 09:05:14 +0200

    lab 1

---
+++ lab_1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ProductsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lab_1/src/app/model/product.ts
@@ -0,0 +1,12 @@
+export class proudact{
+    id:number=1;
+    name:string="";
+    quantity:number=1;
+    price:number=1;
+    img:string="";
+}
+// i)	ID – of type number.
+// ii)	Name – of type string
+// iii)	Quantity – Of type number.
+// iv)	Price – of type number.
+// v)	Img – of type string
